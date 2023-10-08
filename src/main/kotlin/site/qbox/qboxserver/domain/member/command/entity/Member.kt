98d70fd20162841b803f414a5481465f85928881
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-08 21:07:36 +0900

    feat : member entity 구현

---
+++ src/main/kotlin/site/qbox/qboxserver/domain/member/command/entity/Member.kt
@@ -0,0 +1,34 @@
+package site.qbox.qboxserver.domain.member.command.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.springframework.security.crypto.password.PasswordEncoder
+import site.qbox.qboxserver.domain.member.command.exception.NotMatchPasswordException
+
+@Entity
+class Member(
+    @Id val email: String,
+    @Column(nullable = false, unique = true) var nickname: String,
+    @Column(nullable = false) var password: String,
+    passwordEncoder: PasswordEncoder,
+    ) {
+    val role: Set<Role> = setOf(Role.USER)
+    init {
+        this.password = passwordEncoder.encode(password)
+    }
+
+    fun changePassword(beforePassword: String, newPassword: String, passwordEncoder: PasswordEncoder) {
+        if (!passwordEncoder.matches(beforePassword, this.password)) {
+            throw NotMatchPasswordException()
+        }
+        this.password = passwordEncoder.encode(newPassword)
+    }
+
+    fun changeNickname(newNickname: String) {
+        this.nickname = newNickname
+    }
+
+    val emailDomain: String
+        get() = email.split("@")[1]
+}
