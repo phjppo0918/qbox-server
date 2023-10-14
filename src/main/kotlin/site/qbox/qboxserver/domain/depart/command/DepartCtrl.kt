036a6edac47ede2b72b95b4af5b836f6463feb8b
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-14 18:57:12 +0900

    fix : depart 생성 시 응답 코드 변경

--- src/main/kotlin/site/qbox/qboxserver/domain/depart/command/DepartCtrl.kt
+++ src/main/kotlin/site/qbox/qboxserver/domain/depart/command/DepartCtrl.kt
@@ -1,10 +1,8 @@
 package site.qbox.qboxserver.domain.depart.command
 
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 import site.qbox.qboxserver.domain.depart.command.dto.CreateDepartReq
 import site.qbox.qboxserver.global.dto.IdRes
 
@@ -14,6 +12,6 @@ class DepartCtrl(
     private val departSvc: DepartSvc,
 ) {
     @PostMapping
-    fun createDepart(@RequestBody @Valid req: CreateDepartReq) : IdRes<Long>
-     = departSvc.create(req)
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createDepart(@RequestBody @Valid req: CreateDepartReq): IdRes<Long> = departSvc.create(req)
 }
