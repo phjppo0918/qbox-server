b0078cd655ff16c5d5491324ad044edef85beef5
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-14 19:50:05 +0900

    test : controller 관련 test 작성

---
+++ src/test/kotlin/site/qbox/qboxserver/domain/lecture/command/LectureCtrlTest.kt
@@ -0,0 +1,42 @@
+package site.qbox.qboxserver.domain.lecture.command
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.restdocs.payload.JsonFieldType
+import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.payload.PayloadDocumentation.requestFields
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import site.qbox.qboxserver.config.WebClientDocsTest
+import site.qbox.qboxserver.domain.lecture.command.dto.CreateLectureReq
+
+@WebMvcTest(LectureCtrl::class)
+class LectureCtrlTest : WebClientDocsTest() {
+
+    @MockBean
+    private lateinit var lectureSvc: LectureSvc
+
+    init {
+        it("lecture 생성을 수행한다") {
+            val req = CreateLectureReq("ABC123", "자료구조", 4)
+
+            val action = performPost("/lectures", req)
+
+            action.andExpect(MockMvcResultMatchers.status().isCreated())
+
+            action.andDo(
+                print(
+                    "create-lecture",
+                    requestFields(
+                        fieldWithPath("code").type(JsonFieldType.STRING)
+                            .description("과목 코드"),
+                        fieldWithPath("name").type(JsonFieldType.STRING)
+                            .description("과목 이름"),
+                        fieldWithPath("departId").type(JsonFieldType.NUMBER)
+                            .description("학과 ID"),
+                    )
+                )
+            )
+        }
+    }
+
+}
